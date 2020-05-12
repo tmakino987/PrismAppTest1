e2146a5fc4623b4c2ba574f142d731db5be57dc7
toshiki makino <ramen_family@yahoo.co.jp>
2020-05-12 22:56:53 +0900

    fiest commit

---
+++ PrismAppTest1/PrismAppTest1/PrismAppTest1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrismAppTest1.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            LabelC = "Test";
+
+            ButtonCCommand = new DelegateCommand(relayButtonC);
+        }
+
+        private string _labelC = string.Empty;
+        public string LabelC
+        {
+            get { return _labelC; }
+            set { SetProperty(ref _labelC, value); }
+        }
+
+        public DelegateCommand ButtonCCommand { get; set; }
+
+        private void relayButtonC()
+        {
+            LabelC = "EEE";
+        }
+    }
+}
